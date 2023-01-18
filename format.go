eaad06ecee6de01af1312c3b92e40a5358e891ff
Phil Hanna <ph1204@gmail.com>
2023-01-18 10:47:12 -0500

    Added comments

--- format.go
+++ format.go
@@ -2,16 +2,19 @@ package commas
 
 import "fmt"
 
+// Format returns the comma-separated value of an int
 func Format(d int) string {
 	s := fmt.Sprint(d)
 	return format(s)
 }
 
+// Format64 returns the comma-separated value of an int64
 func Format64(d int64) string {
 	s := fmt.Sprint(d)
 	return format(s)
 }
 
+// format is an internal function used by Format and Format64
 func format(s string) string {
 	sb := ""
 	m := 0
